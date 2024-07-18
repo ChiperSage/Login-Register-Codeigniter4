d83d70fb25b7d31fabc08272fe049a2aec8a3287
ByteNinja <pijayblog@gmail.com>
2024-07-18 14:19:33 +0700

    Create UserModel.php

---
+++ app/models/UserModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['username', 'password', 'email', 'login_attempts', 'last_login_attempt', 'remember_me_token'];
+    protected $beforeInsert = ['hashPassword'];
+    protected $beforeUpdate = ['hashPassword'];
+
+    protected function hashPassword(array $data)
+    {
+        if (!isset($data['data']['password']) || empty($data['data']['password'])) {
+            return $data;
+        }
+        $data['data']['password'] = password_hash($data['data']['password'], PASSWORD_DEFAULT);
+        return $data;
+    }
+}
