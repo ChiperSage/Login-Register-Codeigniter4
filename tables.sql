325645d12ce1d3aedc92bdf55d47a9584c120ffd
ByteNinja <pijayblog@gmail.com>
2024-07-18 14:25:57 +0700

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -18,7 +18,7 @@ CREATE TABLE users (
 
 -- Create the 'roles' table
 CREATE TABLE roles (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    role_id INT AUTO_INCREMENT PRIMARY KEY,
     role_name VARCHAR(100) NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -26,7 +26,7 @@ CREATE TABLE roles (
 
 -- Create the 'groups' table
 CREATE TABLE groups (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    group_id INT AUTO_INCREMENT PRIMARY KEY,
     role_id INT,
     user_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
