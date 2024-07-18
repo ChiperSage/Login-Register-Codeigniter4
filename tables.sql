7942782df40147f109637d7beb064a448d04a7c3
ByteNinja <pijayblog@gmail.com>
2024-07-18 14:26:08 +0700

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -4,7 +4,7 @@
 
 -- Create the 'users' table
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     login_attempts INT DEFAULT 0,
