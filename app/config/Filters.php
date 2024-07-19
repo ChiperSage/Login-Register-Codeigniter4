8199945f06f9b9c423be1741e6968678fd3dbc67
ByteNinja <pijayblog@gmail.com>
2024-07-19 15:55:53 +0700

    Create Filters.php

---
+++ app/config/Filters.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class Filters extends BaseConfig
+{
+    public $aliases = [
+        'csrf'     => \CodeIgniter\Filters\CSRF::class,
+        'toolbar'  => \CodeIgniter\Filters\DebugToolbar::class,
+        'honeypot' => \CodeIgniter\Filters\Honeypot::class,
+        'auth'     => \App\Filters\AuthFilter::class, // Register the auth filter
+    ];
+
+    public $globals = [
+        'before' => [
+            // 'honeypot',
+            // 'csrf',
+        ],
+        'after'  => [
+            'toolbar',
+            // 'honeypot',
+        ],
+    ];
+
+    public $methods = [];
+
+    public $filters = [
+        'auth' => ['before' => ['users/*']], // Apply the auth filter to all routes under 'users'
+    ];
+}
