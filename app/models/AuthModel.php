121b740619570decc7241cdf26c1ceba63b7b767
ByteNinja <pijayblog@gmail.com>
2024-07-19 15:48:31 +0700

    Update AuthModel.php

--- app/models/AuthModel.php
+++ app/models/AuthModel.php
@@ -7,14 +7,14 @@ use CodeIgniter\Model;
 class AuthModel extends Model
 {
     protected $table = 'users';
-    protected $primaryKey = 'id';
-    protected $allowedFields = ['username', 'password', 'login_attempts', 'last_login_attempt', 'remember_me_token'];
+    protected $primaryKey = 'user_id';
+    protected $allowedFields = ['username', 'password', 'email', 'login_attempts', 'last_login_attempt', 'remember_me_token'];
     protected $beforeInsert = ['hashPassword'];
     protected $beforeUpdate = ['hashPassword'];
 
     protected function hashPassword(array $data)
     {
-        if (!isset($data['data']['password'])) {
+        if (!isset($data['data']['password']) || empty($data['data']['password'])) {
             return $data;
         }
         $data['data']['password'] = password_hash($data['data']['password'], PASSWORD_DEFAULT);
