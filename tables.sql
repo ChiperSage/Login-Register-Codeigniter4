617e2ad33f8d9abd4fd7043b70f0c1761efc8b1c
ByteNinja <pijayblog@gmail.com>
2024-08-05 17:11:56 +0700

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,24 +1,18 @@
--- CREATE DATABASE myapp;
-
--- USE myapp;
-
 -- Create the 'users' table
 CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     login_attempts INT DEFAULT 0,
-    last_login_attempt INT DEFAULT 0,
+    last_login_attempt TIMESTAMP DEFAULT NULL,
     remember_me_token VARCHAR(255) DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (group_id) REFERENCES groups(id)
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- Create the 'roles' table
 CREATE TABLE roles (
-    role_id INT AUTO_INCREMENT PRIMARY KEY,
+    role_id SERIAL PRIMARY KEY,
     role_name VARCHAR(100) NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -26,9 +20,12 @@ CREATE TABLE roles (
 
 -- Create the 'groups' table
 CREATE TABLE groups (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     role_id INT,
     user_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (role_id) REFERENCES roles(role_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    UNIQUE (role_id, user_id)
 );
